afaa05d3ce199956b9c6edb8613993397757b72b
Ramakrishna Penti <ramakrishna.penti@wavelabs.ai>
2022-12-29 12:54:27 +0530

    all code

---
+++ src/main/java/com/example/demo/Firstexample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Firstexample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Firstexample1Application.class, args);
+	}
+
+}
