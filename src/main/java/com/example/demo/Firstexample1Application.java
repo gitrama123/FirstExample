7119352dba9ae64caa03a8327c12ae1fbe3c9031
Ramakrishna Penti <ramakrishna.penti@wavelabs.ai>
2022-12-29 15:14:09 +0530

    after second commit

--- src/main/java/com/example/demo/Firstexample1Application.java
+++ src/main/java/com/example/demo/Firstexample1Application.java
@@ -3,10 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.help.Animal;
+
 @SpringBootApplication
 public class Firstexample1Application {
 
 	public static void main(String[] args) {
+		Animal a=new Animal();
+		a.give();
 		SpringApplication.run(Firstexample1Application.class, args);
 	}
 
---
+++ src/main/java/com/example/demo/help/Animal.java
@@ -0,0 +1,9 @@
+package com.example.demo.help;
+
+public class Animal {
+
+	public void give() {
+		System.out.println("Hello");
+		
+	}
+}
